maka-agent/maka-agent

refactor(headless): extract fixed prompt WAL types
Move fixed-prompt WAL schema constants and event types into a package-local leaf, then migrate production schema consumers to depend on it directly.

Keep controller imports compatible through explicit re-exports and add a source contract for ownership and dependency direction. The JSONL schema and runtime behavior are unchanged.

packages/headless/src/prompt-ab-types.ts
@@ -10,11 +10,8 @@ import type {
   AbTaskComparison,
   AbTaskLevelSummary,
 } from './ab-types.js';
-import type {
-  FixedPromptTask,
-  FixedPromptTaskWalEvent,
-  HarborTaskRunner,
-} from './fixed-prompt-controller.js';
+import type { FixedPromptTask, HarborTaskRunner } from './fixed-prompt-controller.js';
+import type { FixedPromptTaskWalEvent } from './fixed-prompt-wal-types.js';
 
 export interface SummarizePromptAbComparisonInput {
   runId: string;

packages/headless/src/prompt-optimization-loop.ts
@@ -3,18 +3,20 @@ import { isDeepStrictEqual } from 'node:util';
 import type { Config } from './contracts.js';
 import {
   appendFixedPromptWalEvent,
-  FIXED_PROMPT_WAL_SCHEMA_VERSION,
   runFixedPromptController,
   readFixedPromptWal,
   writeFixedPromptResultsTsv,
   type FixedPromptControllerResult,
   type FixedPromptTask,
+  type HarborTaskRunner,
+} from './fixed-prompt-controller.js';
+import {
+  FIXED_PROMPT_WAL_SCHEMA_VERSION,
   type FixedPromptTaskCompletedEvent,
   type FixedPromptTaskWalEvent,
-  type HarborTaskRunner,
   type PromptCandidateRewardHackScan,
   type RsiControllerAttributionEvent,
-} from './fixed-prompt-controller.js';
+} from './fixed-prompt-wal-types.js';
 import {
   extractTrajectoryDigest,
   runPromptCandidateRound,

apache/maka

packages/headless/src/prompt-ab-types.ts
@@ -10,11 +10,8 @@ import type {
   AbTaskComparison,
   AbTaskLevelSummary,
 } from './ab-types.js';
-import type {
-  FixedPromptTask,
-  FixedPromptTaskWalEvent,
-  TaskRunner,
-} from './fixed-prompt-controller.js';
+import type { FixedPromptTask, TaskRunner } from './fixed-prompt-controller.js';
+import type { FixedPromptTaskWalEvent } from './fixed-prompt-wal-types.js';
 
 export interface SummarizePromptAbComparisonInput {
   runId: string;

packages/headless/src/prompt-optimization-loop.ts
@@ -4,19 +4,21 @@ import { isDeepStrictEqual } from 'node:util';
 import type { Config } from './contracts.js';
 import {
   appendFixedPromptWalEvent,
-  FIXED_PROMPT_WAL_SCHEMA_VERSION,
   runFixedPromptController,
   hashSystemPrompt,
   readFixedPromptWal,
   writeFixedPromptResultsTsv,
   type FixedPromptControllerResult,
   type FixedPromptTask,
+  type TaskRunner,
+} from './fixed-prompt-controller.js';
+import {
+  FIXED_PROMPT_WAL_SCHEMA_VERSION,
   type FixedPromptTaskCompletedEvent,
   type FixedPromptTaskWalEvent,
-  type TaskRunner,
   type PromptCandidateRewardHackScan,
   type RsiControllerAttributionEvent,
-} from './fixed-prompt-controller.js';
+} from './fixed-prompt-wal-types.js';
 import {
   extractTrajectoryDigest,
   runPromptCandidateRound,